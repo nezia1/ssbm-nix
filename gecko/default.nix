ea7fa8cecc1df44a67c247a5415b0f53cebe8a74
Anthony Rodriguez <anthony@nezia.dev>
2025-09-01 15:27:37 +0200

    gecko: fix build

--- gecko/default.nix
+++ gecko/default.nix
@@ -1,15 +1,21 @@
-{ stdenv, fetchFromGitHub, buildGoPackage }:
-
-buildGoPackage rec {
+{
+  fetchFromGitHub,
+  buildGoModule,
+}:
+buildGoModule (finalAttrs: {
   pname = "gecko";
   version = "3.4.0";
+  vendorHash = null;
 
   src = fetchFromGitHub {
     owner = "JLaferri";
     repo = "gecko";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     sha256 = "1jqxcimpl58czvxi52jndrnzhhqmg0i4v5dp6amibg2wxwyy12w3";
   };
 
-  goPackagePath = "gecko";
-}
+  # This avoid having to update the vendor hash on every update (see https://nixos.org/manual/nixpkgs/stable/#var-go-vendorHash)
+  postPatch = ''
+    go mod init gecko
+  '';
+})
